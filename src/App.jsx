59ea8953addac520f298a1708e78d48116cf25ad
LuieR-RMT-WDPT-MAY2023 <rhluisenrique@gmail.com>
2023-09-25 16:22:54 -0400

    Done react-tweet

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import "./App.css";
 import Tweet from "./components/Tweet";
 
+
 const tweetsArray = [
+
   {
+    id: 1,
     user: {
       name: "Thoughts of Dog®",
       image:
@@ -14,6 +17,7 @@ const tweetsArray = [
       "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
   },
   {
+    id: 2,
     user: {
       name: "Thoughts of Dog®",
       image:
@@ -25,6 +29,7 @@ const tweetsArray = [
       "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
   },
   {
+    id: 3,
     user: {
       name: "Thoughts of Dog®",
       image:
@@ -38,9 +43,14 @@ const tweetsArray = [
 ];
 
 function App() {
+
+
+
   return (
     <div className="App">
-      <Tweet />
+      { tweetsArray.map((currentTweet, index) => (
+        <Tweet key={index} tweet={currentTweet} />
+      ))}
     </div>
   );
 }
